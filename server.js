aebb521fa7c93a0fe2d327e4f4cdb0dd4ef45c5e
achelars <rachel.n.a.sinaga@gmail.com>
2024-07-10 17:59:27 +0700

    Granted necessary permissions to the database user and ran migrations

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
 const productRoutes = require('./src/routes/productRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const sequelize = new Sequelize('mora_jaya_hub', 'mora_user', 'user1234', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.use(bodyParser.json());
 app.use('/api/products', productRoutes);
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -17,4 +17,22 @@ const Product = sequelize.define('Product', {
   timestamps: true,
 });
 
-module.exports = Product;
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+  }, {});
+  Product.associate = function(models) {
+    // associations can be defined here
+  };
+  return Product;
+};
